7e38468794a43caad5091dee17c14ae55336c0b0
Igor Sfiligoi <isfiligoi@sdsc.edu>
2023-09-10 07:47:28 -0700

    Use rng.choice for fast version

--- biom/_subsample.pyx
+++ biom/_subsample.pyx
@@ -9,6 +9,12 @@
 import numpy as np
 cimport numpy as cnp
 
+ctypedef fused TData:
+    cnp.float64_t
+    cnp.float32_t
+    cnp.int64_t
+
+
 cdef _subsample_with_replacement(cnp.ndarray[cnp.float64_t, ndim=1] data,
                                  cnp.ndarray[cnp.int32_t, ndim=1] indptr,
                                  cnp.int64_t n,
@@ -52,7 +58,7 @@ cdef _subsample_with_replacement(cnp.ndarray[cnp.float64_t, ndim=1] data,
         data[start:end] = rng.multinomial(n, pvals)
 
 
-cdef _subsample_slice_without_replacement(cnp.ndarray[cnp.float64_t, ndim=1] data,
+cdef _subsample_slice_without_replacement(cnp.ndarray[TData, ndim=1] data,
                                           cnp.int32_t start,
                                           cnp.int32_t end,
                                           cnp.int64_t n,
@@ -112,7 +118,7 @@ cdef _subsample_slice_without_replacement(cnp.ndarray[cnp.float64_t, ndim=1] dat
     el = 0         # index in result/data
     count_el = 0  # index in permutted
     count_rem = long(data[start])  # since each data has multiple els, sub count there
-    data[start] = 0.0
+    data[start] = 0
     for idx in range(n):
         perm_count_el = permuted[idx]
         # the array is sorted, so just jump ahead
@@ -120,13 +126,13 @@ cdef _subsample_slice_without_replacement(cnp.ndarray[cnp.float64_t, ndim=1] dat
             count_el += count_rem
             el += 1
             count_rem = long(data[start+el])
-            data[start+el] = 0.0
+            data[start+el] = 0
         count_rem -= (perm_count_el - count_el)
         count_el = perm_count_el
 
         data[start+el] += 1
     # clean up tail elements
-    data[start+el+1:end] = 0.0
+    data[start+el+1:end] = 0
 
 
 cdef _subsample_without_replacement(cnp.ndarray[cnp.float64_t, ndim=1] data,
@@ -243,6 +249,68 @@ cdef _subsample_fast(cnp.ndarray[cnp.float64_t, ndim=1] data,
         data[start:end] = idata
 
 
+cdef _subsample_fast_wo(cnp.ndarray[cnp.float64_t, ndim=1] data,
+                        cnp.ndarray[cnp.int32_t, ndim=1] indptr,
+                        cnp.int64_t n,
+                        object rng):
+    """Subsample non-zero values of a sparse array using an approximate method
+       without replacement.
+
+    Parameters
+    ----------
+    data : {csr_matrix, csc_matrix}.data
+        A 1xM sparse vector data
+    indptr : {csr_matrix, csc_matrix}.indptr
+        A 1xM sparse vector indptr
+    n : int
+        Number of items to subsample from `arr`
+    rng : Generator instance
+        A random generator. This will likely be an instance returned
+        by np.random.default_rng
+
+    Returns
+    -------
+    ndarray
+        Subsampled data
+
+    """
+    cdef:
+        cnp.int64_t counts_sum, int_counts_sum
+        cnp.int64_t rems_sum, int_counts_diff
+        cnp.int32_t start,end,length
+        Py_ssize_t i,j
+        cnp.ndarray[cnp.int64_t, ndim=1] idata
+        cnp.ndarray[cnp.int64_t, ndim=1] rems
+        cnp.ndarray[cnp.float64_t, ndim=1] pvals
+
+    for i in range(indptr.shape[0] - 1):
+        start, end = indptr[i], indptr[i+1]
+        length = end - start
+
+        idata = data[start:end].astype(np.int64)
+        counts_sum = idata.sum()
+
+        # TODO: This could potentially be relaxed
+        if counts_sum < n:
+            data[start:end] = 0
+            continue
+
+        # get get int truncation of the fraction as the baseline
+        data[start:end] = (n*idata) // counts_sum
+        int_counts_sum = data[start:end].sum()
+
+        # see how much off we are
+        int_counts_diff = n-int_counts_sum
+        if (int_counts_diff>0):
+            # randomly pick elements from the remainders
+            rems = (n*idata) % counts_sum
+            rems_sum = rems.sum()
+            pvals = rems / rems_sum
+            picked = rng.choice(length, int_counts_diff, replace=False, shuffle=False, p=pvals)
+            for j in range(len(picked)):
+                data[start+picked[j]] += 1
+
+
 cdef _subsample_fast_wr(cnp.ndarray[cnp.float64_t, ndim=1] data,
                         cnp.ndarray[cnp.int32_t, ndim=1] indptr,
                         cnp.int64_t n,
@@ -337,5 +405,5 @@ def _subsample(arr, n, with_replacement, rng, true_subsample):
     elif (true_subsample):
        return _subsample_without_replacement(arr.data, arr.indptr, n, rng)
     else:
-       return _subsample_fast(arr.data, arr.indptr, n, rng)
+       return _subsample_fast_wo(arr.data, arr.indptr, n, rng)
 
